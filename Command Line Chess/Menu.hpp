c1e564b74e15abd42b703ffc3f30ebaeb275a537
Zachari <zachariwils@gmail.com>
2022-02-20 18:52:50 -0500

    Add project files.

---
+++ Command Line Chess/Menu.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdlib.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Menu
+{
+private:
+	~Menu();
+public:
+	Menu();
+	void printMenu(); //to print the menu options stored in vOptions
+
+};
+
